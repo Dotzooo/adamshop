ec4d6d2c9c4f07a7bf69155749ae6e634e2e8222
Dotzooo <projectnba77@gmail.com>
2023-06-07 10:20:34 +0800

    feat: 跨元件訊息

--- src/store/messageStore.js
+++ src/store/messageStore.js
@@ -14,11 +14,14 @@ export const initState = {
 export const messageReducer = (state, action) => {
   switch (action.type) {
     case "POST_MESSAGE":
+      // return {
+      //   type: "success",
+      //   title: "成功",
+      //   text: "這是一段成功的訊息",
+      // };
       return {
-        type: "success",
-        title: "成功",
-        text: "這是一段成功的訊息",
-      };
+        ...action.payload
+      }
     case "CLEAR_MESSAGE":
       return {
         ...initState,
@@ -27,3 +30,36 @@ export const messageReducer = (state, action) => {
       return state;
   }
 };
+
+
+export function handleSuccessMessage(dispatch, res) {
+  dispatch({
+    type: "POST_MESSAGE",
+    payload: {
+      type: "success",
+      title: "更新成功",
+      text: res.data.message,
+    },
+  });
+  
+  setTimeout(() => {
+    dispatch({ type: "CLEAR_MESSAGE" })
+  }, 3000)
+}
+
+export function handleErrorMessage(dispatch, error) {
+  dispatch({
+    type: "POST_MESSAGE",
+    payload: {
+      type: "danger",
+      title: "失敗",
+      text: Array.isArray(error?.response?.data?.message)
+        ? error?.response?.data?.message.join(",")
+        : error?.response?.data?.message,
+    },
+  });
+  
+  setTimeout(() => {
+    dispatch({ type: "CLEAR_MESSAGE" })
+  }, 3000)
+}
